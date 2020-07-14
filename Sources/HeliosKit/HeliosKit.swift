b1a66d68a3f2681923709e8b5e7c1fdd05e3f1d1
Danny Ruiz <ynnadrules@gmail.com>
2020-07-14 13:21:49 -0500

    Rough basic boot/configuration support for crossplatform integration

--- Sources/HeliosKit/HeliosKit.swift
+++ Sources/HeliosKit/HeliosKit.swift
@@ -4,10 +4,17 @@
 @_exported import JavaCoder
 @_exported import AnyCodable
 
-public struct Helios {
-	public static func bootstrap(cachePath: String) {
-		 setenv("TMPDIR", cachePath, 1)
-		 // Required for SSL to work
-		 setenv("URLSessionCertificateAuthorityInfoFile", cachePath + "/cacert.pem", 1)
+var AndroidPackage: String = ""
+
+public protocol Bootable {
+	static func boot(packageName: String, cachePath: String)
+}
+
+public extension Bootable {
+	public static func boot(packageName: String, cachePath: String) {
+		AndroidPackage = packageName
+		setenv("TMPDIR", cachePath, 1)
+		// Required for SSL to work
+		setenv("URLSessionCertificateAuthorityInfoFile", cachePath + "/cacert.pem", 1)
 	}
 }
