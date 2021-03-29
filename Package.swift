6276c7ec800374adc80f4b1ca9429404863fbc9c
Danny Ruiz <ynnadrules@gmail.com>
2021-03-29 12:40:27 -0500

    Restore 32-bit support with latest in kotlin-support branch of swift-java-coder

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/readdle/java_swift", .upToNextMinor(from: "2.1.9")),
 		.package(url: "https://github.com/readdle/swift-java", .upToNextMinor(from: "0.2.4")),
-		.package(url: "https://github.com/readdle/swift-java-coder", .branch("dev/kotlin-support")),
+		.package(url: "https://github.com/heliumfoot/swift-java-coder", .branch("feature/kotlin-support")),
+//		.package(path: "../swift-java-coder"),
 		.package(url: "https://github.com/readdle/swift-anycodable.git", .upToNextMinor(from: "1.0.3"))
 	],
 	targets: [
