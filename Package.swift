1de19b07746a69b13f6d3935a7c91ba1c238c3db
ella_mi <elvira@heliumfoot.com>
2024-01-30 21:11:13 -0600

    Upgrade versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,14 +11,14 @@ let package = Package(
 			targets: ["HeliosKit"]),
 	],
 	dependencies: [
-        .package(name: "java_swift", url: "https://github.com/readdle/java_swift", .upToNextMinor(from: "2.1.9")),
+        .package(name: "java_swift", url: "https://github.com/readdle/java_swift", .upToNextMinor(from: "2.2.0")),
 //		.package(path: "../java_swift"),
-        .package(name: "Java", url: "https://github.com/readdle/swift-java", .upToNextMinor(from: "0.2.5")),
+        .package(name: "Java", url: "https://github.com/readdle/swift-java", .upToNextMinor(from: "2.0.0")),
 //		.package(path: "../swift-java"),
-        .package(name: "JavaCoder", url: "https://github.com/heliumfoot/swift-java-coder", from: "1.0.20"),
+		.package(name: "JavaCoder", url: "https://github.com/readdle/swift-java-coder", .revision("b0b66fa90d7b04c7214da15a17ec5ba14df61c8d")),
 //            .package(name: "JavaCoder", url: "https://github.com/readdle/swift-java-coder", .branch("dev/kotlin-support")),
 //        .package(name: "JavaCoder", path: "../swift-java-coder"),
-        .package(name: "AnyCodable", url: "https://github.com/readdle/swift-anycodable.git", .upToNextMinor(from: "1.1.0"))
+        .package(name: "AnyCodable", url: "https://github.com/readdle/swift-anycodable.git", .upToNextMinor(from: "1.0.2"))
 	],
 	targets: [
 		.target(
